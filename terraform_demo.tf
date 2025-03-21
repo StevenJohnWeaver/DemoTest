223866f00c31aac134889667c87d00c6e4fe32b3
StevenJohnWeaver <stevenjweaver@gmail.com>
2025-03-21 15:01:56 -0400

    Update terraform_demo.tf

--- terraform_demo.tf
+++ terraform_demo.tf
@@ -1,22 +1,27 @@
-variable "vpc_id" {
-  type = string
-  default = "vpc-07b06a38cc5697c2c"
-}
-
-variable "security_group_id" {
-  type = string
-  default = "sg-0d6b3246daec36e9c"
-}
-
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "102.0.0.0/16"
+  tags = {
+    Name = "My-VPC"
+  }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "102.0.0.0/24"
+  tags = {
+    Name = "My-Subnet"
+  }
 }
 
 resource "aws_instance" "my_first_ec2_instance" {
   ami = "ami-08b5b3a93ed654d19" 
   instance_type = "t2.micro" # Got to https://aws.amazon.com/ec2/instance-types/t2/ for a full T2 instance type list.
-  vpc_security_group_ids = [var.security_group_id]
-  vpc_id                 = var.vpc_id
+  vpc_id = aws_vpc.my_vpc.id
+  subnet_id = aws_subnet.my_subnet.id
   tags = {
     Name = "my_first_ec2_instance"
   }

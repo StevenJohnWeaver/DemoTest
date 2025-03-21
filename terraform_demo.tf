b3a7a053f21321061d8babbda3130a2a02833792
StevenJohnWeaver <stevenjweaver@gmail.com>
2025-03-21 15:22:21 -0400

    Update terraform_demo.tf

--- terraform_demo.tf
+++ terraform_demo.tf
@@ -35,34 +35,3 @@ resource "aws_instance" "my_first_ec2_instance" {
     Name = "my_first_ec2_instance"
   }
 }
-
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] #Restrict this in production!
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] #Restrict this in production!
-  }
-}
-
-resource "aws_instance" "my_first_ec2_instance" {
-  ami                    = "ami-08b5b3a93ed654d19" #Check AMI for region!
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-  subnet_id             = aws_subnet.my_subnet.id
-  tags = {
-    Name = "my_first_ec2_instance"
-  }
-}

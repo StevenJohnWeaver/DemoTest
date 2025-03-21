880ad05bde5d6e3991e706f25cb1a150053a26c0
StevenJohnWeaver <stevenjweaver@gmail.com>
2025-03-21 15:20:02 -0400

    Update terraform_demo.tf

--- terraform_demo.tf
+++ terraform_demo.tf
@@ -1,11 +1,11 @@
 variable "vpc_id" {
   type = string
-  default = "Default"
+  default = "Default" #This is not used, remove it.
 }
 
 variable "subnet_id" {
   type = string
-  default = "Default"
+  default = "Default" #This is not used, remove it.
 }
 
 provider "aws" {
@@ -13,7 +13,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "102.0.0.0/16"
+  cidr_block = "10.0.0.0/16" # Changed to a valid CIDR range
   tags = {
     Name = "My-VPC"
   }
@@ -21,17 +21,47 @@ resource "aws_vpc" "my_vpc" {
 
 resource "aws_subnet" "my_subnet" {
   vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "102.0.0.0/24"
+  cidr_block = "10.0.1.0/24" # Changed to a subnet within the VPC's CIDR range
   tags = {
     Name = "My-Subnet"
   }
 }
 
 resource "aws_instance" "my_first_ec2_instance" {
-  ami = "ami-08b5b3a93ed654d19" 
-  instance_type = "t2.micro" # Got to https://aws.amazon.com/ec2/instance-types/t2/ for a full T2 instance type list.
-  vpc_id = aws_vpc.my_vpc.id
-  subnet_id = aws_subnet.my_subnet.id
+  ami           = "ami-08b5b3a93ed654d19" #Check if this is appropriate for your region.
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.my_subnet.id
+  tags = {
+    Name = "my_first_ec2_instance"
+  }
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #Restrict this in production!
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] #Restrict this in production!
+  }
+}
+
+resource "aws_instance" "my_first_ec2_instance" {
+  ami                    = "ami-08b5b3a93ed654d19" #Check AMI for region!
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id             = aws_subnet.my_subnet.id
   tags = {
     Name = "my_first_ec2_instance"
   }

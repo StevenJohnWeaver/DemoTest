e01b7fa28c0e54c3cc10634ca55bbf97430586d1
StevenJohnWeaver <stevenjweaver@gmail.com>
2025-03-21 14:32:04 -0400

    Update terraform_demo.tf

--- terraform_demo.tf
+++ terraform_demo.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_first_ec2_instance" {
-  ami = "ami-123abc321cba18" 
+  ami = "ami-08b5b3a93ed654d19" 
   instance_type = "t2.micro" # Got to https://aws.amazon.com/ec2/instance-types/t2/ for a full T2 instance type list.
   
   tags = {
